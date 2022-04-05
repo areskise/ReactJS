86c184fdd81456c93f5c846daa330aedd2289b52
Ares Kise <areskise@gmail.com>
2022-04-05 21:42:29 +0700

    commit 3

---
+++ src/StaffList/StaffListComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class StaffList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+           
+        }
+    }
+    render() {
+        return
+      }
+}
+
+export default StaffList;
