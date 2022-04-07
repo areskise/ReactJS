94e37505e3159765a59df786cb292df389e0c634
Ares Kise <areskise@gmail.com>
2022-04-07 21:52:58 +0700

    Updated

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -7,28 +7,33 @@ class StaffList extends Component {
         super(props);
 
         this.state = {
-           selectedStaff: null
+           selectedStaff: null,
+           colDefault: "col-12 col-md-6 col-lg-4",
         }
     }
 
     onStaffSelect(staff) {
         this.setState({selectedStaff: staff});
     }
+    numCol(col) {
+        this.setState({colDefault: col});
+    }
 
     renderStaff(staff) {
-        console.log(staff)
         if (staff != null) {
             return (
-                <Card className="col-12">
-                    <CardBody>
-                        <CardTitle>Họ và tên: {staff.name}</CardTitle>
-                        <CardText>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
-                        <CardText>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
-                        <CardText>Phòng ban: {staff.department.name}</CardText>
-                        <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
-                        <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
-                    </CardBody>
-                </Card>
+                <div className="col-12">
+                    <Card>
+                        <CardBody>
+                            <CardTitle>Họ và tên: {staff.name}</CardTitle>
+                            <CardText>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
+                            <CardText>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
+                            <CardText>Phòng ban: {staff.department.name}</CardText>
+                            <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
+                            <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
             );
         } else {
             return <div></div>;
@@ -38,8 +43,8 @@ class StaffList extends Component {
     render() {
         const staffList = this.props.staffs.map((staff) => {
             return (
-                <div key={staff.id} className="col-12 col-md-6 col-lg-4">
-                    <Card onClick={() => this.onStaffSelect(staff)}>
+                <div key={staff.id} className={this.state.colDefault}>
+                    <Card onClick={() => this.onStaffSelect(staff)} className="hover">
                         <CardBody>
                             <CardTitle>{(staff.id) + 1}. {staff.name}</CardTitle>
                         </CardBody>
@@ -51,6 +56,14 @@ class StaffList extends Component {
 
         return (
             <div className="container">
+                <div className="row">
+                    <button className="btn btn-warning col" onClick={() => this.numCol("col-12 col-md-6 col-lg-4")}>mặc định</button>
+                    <button className="btn btn-warning col" onClick={() => this.numCol("col-md-12")}>1 cột</button>
+                    <button className="btn btn-warning col" onClick={() => this.numCol("col-md-6")}>2 cột</button>
+                    <button className="btn btn-warning col" onClick={() => this.numCol("col-md-4")}>3 cột</button>
+                    <button className="btn btn-warning col" onClick={() => this.numCol("col-md-3")}>4 cột</button>
+                    <button className="btn btn-warning col" onClick={() => this.numCol("col-md-2")}>6 cột</button>
+                </div>
                 <div className="row">
                     {staffList}
                 </div>
