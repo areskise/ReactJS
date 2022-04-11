5a3fddcd8643cff548e3d090c583fcfb9bf018e3
Ares Kise <areskise@gmail.com>
2022-04-11 23:49:15 +0700

    Commit 1

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,77 +1,41 @@
-import React, { Component } from 'react';
-import { Card, CardText, CardBody, CardTitle } from 'reactstrap';
-import dateFormat from 'dateformat';
+import React from 'react';
+import { Card, CardImg, CardText, BreadcrumbItem, Breadcrumb } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-class StaffList extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-           selectedStaff: null,
-           colDefault: "col-12 col-md-6 col-lg-4",
-        }
+    function RenderStaffListItem({ staff }) {
+        return(
+            <Card>
+                <Link to={`/Nhân-Viên/${staff.id}`}>
+                <CardImg width="100%" src={staff.image} alt={staff.name} />
+                <CardText className="text-center m-2">{staff.name}</CardText>
+                </Link>
+            </Card>
+        );
     }
 
-    onStaffSelect(staff) {
-        this.setState({selectedStaff: staff});
-    }
-    numCol(col) {
-        this.setState({colDefault: col});
-    }
+    const StaffList = (props) => {
 
-    renderStaff(staff) {
-        if (staff != null) {
+        const staffList = props.staffs.map((staff) => {
             return (
-                <div className="col-12">
-                    <Card>
-                        <CardBody>
-                            <CardTitle>Họ và tên: {staff.name}</CardTitle>
-                            <CardText>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
-                            <CardText>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
-                            <CardText>Phòng ban: {staff.department.name}</CardText>
-                            <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
-                            <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
-                        </CardBody>
-                    </Card>
+                <div key={staff.id} className="col-6 col-md-4 col-lg-2 mb-3">
+                    <RenderStaffListItem staff={staff} />
                 </div>
             );
-        } else {
-            return <div></div>;
-        }
-    }
-
-    render() {
-        const staffList = this.props.staffs.map((staff) => {
-            return (
-                <div key={staff.id} className={this.state.colDefault}>
-                    <Card onClick={() => this.onStaffSelect(staff)} className="hover">
-                        <CardBody>
-                            <CardTitle>{(staff.id) + 1}. {staff.name}</CardTitle>
-                        </CardBody>
-                    </Card>
-                </div>
-            )
-
         });
 
         return (
             <div className="container">
-                <div className="row">
-                    <button className="btn btn-warning col" onClick={() => this.numCol("col-12 col-md-6 col-lg-4")}>mặc định</button>
-                    <button className="btn btn-warning col" onClick={() => this.numCol("col-md-12")}>1 cột</button>
-                    <button className="btn btn-warning col" onClick={() => this.numCol("col-md-6")}>2 cột</button>
-                    <button className="btn btn-warning col" onClick={() => this.numCol("col-md-4")}>3 cột</button>
-                    <button className="btn btn-warning col" onClick={() => this.numCol("col-md-3")}>4 cột</button>
-                    <button className="btn btn-warning col" onClick={() => this.numCol("col-md-2")}>6 cột</button>
+                <div className="row m-0">
+                    <Breadcrumb className="m-0">
+                        <BreadcrumbItem><Link to='/Nhân-Viên'>Nhân Viên</Link></BreadcrumbItem>
+                    </Breadcrumb>
                 </div>
+                <hr />
                 <div className="row">
                     {staffList}
-                </div>
-                <div className="row">
-                    {this.renderStaff(this.state.selectedStaff)}
                 </div>
             </div>
         );
     }
-}
+
 export default StaffList;
