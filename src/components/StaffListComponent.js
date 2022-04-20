c85d98fae3649828877b118382747205a91df9b3
Ares Kise <areskise@gmail.com>
2022-04-20 22:23:18 +0700

    Criterion 1

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
-import { Card, CardImg, CardText, BreadcrumbItem, Breadcrumb } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, BreadcrumbItem, Breadcrumb, Form, FormGroup, Col, Input, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 //Render danh sách nhân viên
-function RenderStaffListItem({ staff }) {
+const RenderStaffListItem = ({ staff }) => {
     return(
         <Card>
             <Link to={`/Nhân-Viên/${staff.id}`}>
@@ -14,68 +14,78 @@ function RenderStaffListItem({ staff }) {
     );
 }
 
-function StaffList(props) {
-
-    //Dùng Hook useState để Render lại khi keyword được thay đổi
-    const [keyword, setName] = useState("");
-
-    //Dùng Hook useState để Render lại khi sortStaff được thay đổi
-    const [sortStaff, setSort] = useState(false);
-
-    //Sắp xếp lại danh sách theo mã số nhân viên khi sortStaff được thay đổi
-    const staffList = props.staffs.sort((a,b) => {
-        if (sortStaff) {
-            return b.id - a.id;
-        } else {
-            return a.id - b.id;
+class StaffList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            keywords: "",
         }
 
-    //Dùng filter() để lọc ra những nhân viên trong tên có từ khóa tìm kiếm
-    }).filter((staff) => {
-        if(keyword === "") {
-            return staff;
-        } else if(staff.name.toLowerCase().includes(keyword.toLowerCase())) {
-            return staff;
-        }
+        this.handleSearch = this.handleSearch.bind(this);
+    }
 
-    //Dùng map() để lặp qua từng nhân viên và lấy chúng vào danh sách 
-    }).map((staff) => {
+    handleSearch(event) {
+        const keyword = event.target.keyword.value;
+        this.setState({ keywords: keyword });
+        event.preventDefault();
+    }
+
+    render() {
+
+        const staffList = this.props.staffs.filter((staff) => {
+            if(this.state.keywords === "") {
+                return staff;
+            } else if(staff.name.toLowerCase().includes(this.state.keywords.toLowerCase())) {
+                return staff;
+            }
+
+        //Dùng map() để lặp qua từng nhân viên và lấy chúng vào danh sách 
+        }).map((staff) => {
             return (
                 <div key={staff.id} className="col-6 col-md-4 col-lg-2 mb-3">
                     <RenderStaffListItem staff={staff} />
                 </div>
             );
-    });
+        });
 
-    
-    //Render toàn bộ giao diện của trang nhân viên
-    return (
-        <div className="container mt-3">
-            <div className="row m-0">
-                <Breadcrumb className="m-0">
-                    <BreadcrumbItem><Link to='/Nhân-Viên'>Nhân Viên</Link></BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col">
-                <button className="btn btn-warning" onClick={() => setSort(!sortStaff)}>Sắp xếp theo mã số nhân viên</button>
+        //Render toàn bộ giao diện của trang nhân viên
+        return (
+            <div className="container mt-3">
+                <div className="row m-0">
+                    <Breadcrumb className="m-0">
+                        <BreadcrumbItem><Link to='/Nhân-Viên'>Nhân Viên</Link></BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col">
+                    <button className="btn btn-warning">
+                        <span className="fa fa-plus fa-lg"></span>
+                    </button>
+                    </div>
+                    
+                    <Form onSubmit={this.handleSearch} className="col-md-8 mb-0">
+                        <FormGroup className="row mb-0">
+                            <Col className="col p-0">
+                                <Button type="submit" className="btn btn-warning">
+                                    Tìm Kiếm
+                                </Button>
+                            </Col>
+                            <Col className="col p-0">
+                                <Input
+                                    type="text"
+                                    name="keyword"
+                                    className="form-control"
+                                    placeholder="Nhập từ khóa tìm kiếm..."
+                                />
+                            </Col>
+                        </FormGroup>
+                    </Form>
                 </div>
-                
-                {/* Tìm kiếm khi value được thay đổi*/}
-                <div className="col p-0">
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Nhập từ khóa tìm kiếm..."
-                        value={ keyword }
-                        onChange={(event) => setName(event.target.value)}
-                        />
+                <hr />
+                <div className="row">
+                    {staffList}
                 </div>
             </div>
-            <hr />
-            <div className="row">
-                {staffList}
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default StaffList;
